aec6e302eda0281be34fda24ccc2508ad908518f
Efren <exg22@hotmail.com>
2021-12-20 23:46:24 -0300

    listar productos

---
+++ src/components/HeaderApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+
+export const HeaderApp = () => (
+        <Col xs={12}>
+          <div>E-Commerce-Polo</div>
+        </Col>
+    );
+
