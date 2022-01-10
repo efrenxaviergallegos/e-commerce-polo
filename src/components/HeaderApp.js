186737b713761a6e4ef29ed1b7fe9870f6125ee4
efrenxaviergallegos <exg22@hotmail.com>
2022-01-09 22:18:46 -0300

    Revert "listar productos"

--- src/components/HeaderApp.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Col } from 'react-bootstrap';
-
-export const HeaderApp = () => (
-        <Col xs={12}>
-          <div>E-Commerce-Polo</div>
-        </Col>
-    );
-
